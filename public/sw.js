aad85535b8ff532ae0087c0d00f17df263945ab7
vuong-ng <nguyenxuanvuong107003@gmail.com>
2024-10-27 02:10:42 -0500

    add notification

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+console.log("Service Worker Loaded...");
+
+self.addEventListener("push", (e) => {
+  const data = e.data.json();
+  console.log("Push Recieved...");
+
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: "Knock Knock",
+    }
+    )
+  )
+});
